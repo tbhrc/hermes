2959d50c2f276b6039a8f9e988cf1f365255efd9
root <root@srv1601425.hstgr.cloud>
2026-05-04 09:28:37 +0000

    Backup local Hermes tweaks

--- plugins/platforms/teams/adapter.py
+++ plugins/platforms/teams/adapter.py
@@ -589,9 +589,8 @@ class TeamsAdapter(BasePlatformAdapter):
 
 def interactive_setup() -> None:
     """Guide the user through Teams setup using the Teams CLI."""
-    from hermes_cli.config import (
-        get_env_value,
-        save_env_value,
+    from hermes_cli.config import get_env_value, save_env_value
+    from hermes_cli.cli_output import (
         prompt,
         prompt_yes_no,
         print_info,
